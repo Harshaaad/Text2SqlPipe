d14a59253c2a17c1a9004a95c1f309c8131d2f41
Harshaaad <139134594+Harshaaad@users.noreply.github.com>
2024-10-03 11:23:34 +0530

    Update text_2_sql.py

--- text_2_sql.py
+++ text_2_sql.py
@@ -1,59 +1,64 @@
-"""
-title: Llama Index Ollama Pipeline
-author: open-webui
-date: 2024-05-30
-version: 1.0
-license: MIT
-description: A pipeline for retrieving relevant information from a knowledge base using the Llama Index library with Ollama embeddings.
-requirements: llama-index, llama-index-llms-ollama, llama-index-embeddings-ollama
-"""
-
 from typing import List, Union, Generator, Iterator
-from schemas import OpenAIChatMessage
 import os
-
 from pydantic import BaseModel
+from langchain_community.llms import Ollama
+from langchain_community.utilities import SQLDatabase
+from langchain.chains import create_sql_query_chain
 
 
 class Pipeline:
-
     class Valves(BaseModel):
-        LLAMAINDEX_OLLAMA_BASE_URL: str
-        LLAMAINDEX_MODEL_NAME: str
-        LLAMAINDEX_EMBEDDING_MODEL_NAME: str
+        DB_HOST: str
+        DB_PORT: str
+        DB_USER: str
+        DB_PASSWORD: str
+        DB_DATABASE: str
+        DB_TABLE: str
+        OLLAMA_HOST: str
+        TEXT_TO_SQL_MODEL: str
 
+    # Update valves/ environment variables based on your selected database
     def __init__(self):
-        self.documents = None
-        self.index = None
+        self.name = "Database RAG Pipeline"
+        self.db = None
 
+        # Initialize
         self.valves = self.Valves(
             **{
-                "LLAMAINDEX_OLLAMA_BASE_URL": os.getenv("LLAMAINDEX_OLLAMA_BASE_URL", "http://host.docker.internal:11434"),
-                "LLAMAINDEX_MODEL_NAME": os.getenv("LLAMAINDEX_MODEL_NAME", "llama3.1:8b"),
-                "LLAMAINDEX_EMBEDDING_MODEL_NAME": os.getenv("LLAMAINDEX_EMBEDDING_MODEL_NAME", "nomic-embed-text:latest"),
+                "pipelines": ["*"],  # Connect to all pipelines
+                "DB_HOST": os.getenv("DB_HOST", "localhost"),  # Database hostname (will not work if open-webui runs in docker as docker wont be able to access localhost port)
+                "DB_PORT": os.getenv("DB_PORT", "5432"),  # Database port
+                "DB_USER": os.getenv(
+                    "DB_USER", "postgres"
+                ),  # User to connect to the database with
+                "DB_PASSWORD": os.getenv(
+                    "DB_PASSWORD", "admin"
+                ),  # Password to connect to the database with
+                "DB_DATABASE": os.getenv(
+                    "DB_DATABASE", "postgres"
+                ),  # Database to select on the DB instance
+                "DB_TABLE": os.getenv(
+                    "DB_TABLE", "matches"
+                ),  # Table(s) to run queries against
+                "OLLAMA_HOST": os.getenv(
+                    "OLLAMA_HOST", "http://localhost:11434"
+                ),  # Make sure to update with the URL of your Ollama host, such as http://localhost:11434 or remote server address
+                "TEXT_TO_SQL_MODEL": os.getenv(
+                    "TEXT_TO_SQL_MODEL", "llama3.1:8b"
+                ),  # Model to use for text-to-SQL generation
             }
         )
 
+    def init_db_connection(self):
+        # Update your DB connection string based on selected DB engine - current connection string is for Postgres
+        self.db = SQLDatabase.from_uri(
+            f"postgresql+psycopg2://{self.valves.DB_USER}:{self.valves.DB_PASSWORD}@{self.valves.DB_HOST}:{self.valves.DB_PORT}/{self.valves.DB_DATABASE}"
+        )
+        return self.db
+
     async def on_startup(self):
-        from llama_index.embeddings.ollama import OllamaEmbedding
-        from llama_index.llms.ollama import Ollama
-        from llama_index.core import Settings, VectorStoreIndex, SimpleDirectoryReader
-
-        Settings.embed_model = OllamaEmbedding(
-            model_name=self.valves.LLAMAINDEX_EMBEDDING_MODEL_NAME,
-            base_url=self.valves.LLAMAINDEX_OLLAMA_BASE_URL,
-        )
-        Settings.llm = Ollama(
-            model=self.valves.LLAMAINDEX_MODEL_NAME,
-            base_url=self.valves.LLAMAINDEX_OLLAMA_BASE_URL,
-        )
-
         # This function is called when the server is started.
-        global documents, index
-
-        self.documents = SimpleDirectoryReader("/data").load_data()
-        self.index = VectorStoreIndex.from_documents(self.documents)
-        pass
+        self.init_db_connection()
 
     async def on_shutdown(self):
         # This function is called when the server is stopped.
@@ -62,13 +67,14 @@ class Pipeline:
     def pipe(
         self, user_message: str, model_id: str, messages: List[dict], body: dict
     ) -> Union[str, Generator, Iterator]:
-        # This is where you can add your custom RAG pipeline.
-        # Typically, you would retrieve relevant information from your knowledge base and synthesize it to generate a response.
+        
+        llm = Ollama(
+            model=self.valves.TEXT_TO_SQL_MODEL, base_url=self.valves.OLLAMA_HOST
+        )
+        db = self.db
 
-        print(messages)
-        print(user_message)
+        chain = create_sql_query_chain(llm, db)
+        response = chain.invoke({"question": user_message})
+        sql_query = response.split('SQLQuery: ')[1].strip()
 
-        query_engine = self.index.as_query_engine(streaming=True)
-        response = query_engine.query(user_message)
-
-        return response.response_gen
+        return db.run(sql_query)
